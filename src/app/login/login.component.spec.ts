17851ec2bcb5291f2ec3a42314f7635d84d84827
Jahaziel Basilio Hernandez <66385868+jahazielBH@users.noreply.github.com>
2020-10-13 09:35:49 -0500

    Update login.component.spec.ts

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -19,7 +19,7 @@ describe('LoginComponent', () => {
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
