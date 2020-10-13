e0ea623cbd4b0555bc3e96acb9f9cacc7137af78
jahazielBH <dmotodroide@gmail.com>
2020-10-13 02:05:17 -0500

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { MapComponent } from './map/map.component';
+import { RegistroComponent } from './registro/registro.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component:LoginComponent,
-    
-  }
+  {path: '', pathMatch: 'full', redirectTo: '/landing-page'},
+  {path: 'landing-page', component: LoginComponent},
+  {path: 'mapa', component: MapComponent},
+  {path: 'registro', component: RegistroComponent}
 ];
 
 @NgModule({
