15e9f289fb883dcc0f5f5e6a3905685d16ce1314
GabrielHS19 <jesus.gh72@gmail.com>
2020-10-13 00:12:59 -0500

    agregar registro #9

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,19 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapComponent } from './map/map.component';
-import { MarkerService } from './_services/marker.service';
-import { PopUpService } from './_services/pop-up.service';
-import { ShapeService } from './_services/shape.service';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
   ],
-  providers: [
-    MarkerService,
-    PopUpService,
-    ShapeService
-  ],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
