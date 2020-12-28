a55f0a038bf1f80f606a653ac40358c5475578de
jahazieljbh <62615784+jahazieljbh@users.noreply.github.com>
2020-12-28 13:31:12 -0600

    Closes #23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,14 +9,21 @@ import { LoginComponent } from './login/login.component';
 import { StorageService } from "./services/storage.service";
 import {FormsModule} from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
-
-
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { PmComponent } from './pm/pm.component';
+import { AdminComponent } from './admin/admin.component';
+import { httpInterceptorProviders } from './auth/auth-interceptor';
 @NgModule({
   declarations: [
     AppComponent,
     RegistroComponent,
     LoginComponent,
     HeaderComponent,
+    HomeComponent,
+    UserComponent,
+    PmComponent,
+    AdminComponent,
   
   ],
   imports: [
@@ -25,7 +32,7 @@ import { HeaderComponent } from './header/header.component';
     HttpClientModule,
     FormsModule
   ],
-  providers: [StorageService],
+  providers: [httpInterceptorProviders, StorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
