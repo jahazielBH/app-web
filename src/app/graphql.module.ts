3f12984b1a75a9a0a07e8a565f59870ca052fd1e
GabrielHS19 <jesus.gh72@gmail.com>
2021-01-10 17:42:53 -0600

    add changes graphql port

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = 'http://104.154.247.177:5047/graphql/'; // <-- add the URL of the GraphQL server here
+const uri = 'http://104.154.247.177:5046/graphql/'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
